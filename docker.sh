5ccac10257154163adf9c074bc55ee03ef897bcd
Ravi Lamichhane <ravilamichhane30@gmail.com>
2022-08-15 06:37:45 +0545

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 !#!/bin/sh
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common 
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update
